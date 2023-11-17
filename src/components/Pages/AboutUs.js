adfcb202e10934e3ef5f67fc836a6a05de8b314d
Mathew Puig <puigster956@gmail.com>
2023-11-16 19:32:22 -0800

    demo

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -1,8 +1,8 @@
 function AboutUs() {
     return (
         <div>
-            <div className="AboutUs">
-               <h2>Snack-ade : Donating made sweet & simple</h2>
+            <div >
+               <h2 className="center-title">Snack-ade : Donating made sweet & simple</h2>
                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis leo vitae magna consequat euismod. Fusce id nisi auctor, aliquet nisl at, lacinia augue. Quisque euismod semper lacus, quis tincidunt eros ullamcorper quis. Morbi auctor, nunc quis consequat sagittis, felis nisl vulputate nibh, quis ultrices sapien nisl ac erat. Curabitur quis nisl quis lorem fringilla commodo. Cras vitae nisi ut augue mattis interdum. Mauris quis lacus id leo malesuada mattis. Nunc vitae neque quis massa lobortis blandit. In hac habitasse platea dictumst.
 
                     Suspendisse potenti. Vivamus quis nisl sit amet nisl tincidunt consequat. Phasellus quis lorem sed lorem condimentum tincidunt. Proin quis nisl quis lorem sagittis sollicitudin. Sed at nisl quis lorem fermentum luctus. Donec quis nisl quis lorem consequat tincidunt. Nulla quis nisl quis lorem sagittis sollicitudin. Sed at nisl quis lorem fermentum luctus. Donec quis nisl quis lorem consequat tincidunt. Nulla quis nisl quis lorem sagittis sollicitudin. Sed at nisl quis lorem fermentum luctus.
