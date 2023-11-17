d31476c45fc8a9b20890f28037573193dca76ede
Mathew Puig <puigster956@gmail.com>
2023-11-16 18:58:56 -0800

    fix text

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -7,16 +7,16 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div>
-            <div className="home">
-                <h1 className="">Donating made sweet & simple</h1>
+            <div>
+                <h1 >Donating made sweet & simple</h1>
                 <div className="sub-home">
                     <MyBanner/>
                 </div>
                 
                
-                    <h2>This Month's Best Sellers</h2>
+                    <h2 >This Month's Best Sellers</h2>
                     <Catalog/>
-                    <div className="sub-home">
+                    <div >
                         <h2>Mission Statement</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis leo vitae magna consequat euismod. Fusce id nisi auctor, aliquet nisl at, lacinia augue. Quisque euismod semper lacus, quis tincidunt eros ullamcorper quis. Morbi auctor, nunc quis consequat sagittis, felis nisl vulputate nibh, quis ultrices sapien nisl ac erat. Curabitur quis nisl quis lorem fringilla commodo. Cras vitae nisi ut augue mattis interdum. Mauris quis lacus id leo malesuada mattis. Nunc vitae neque quis massa lobortis blandit. In hac habitasse platea dictumst.
                             Suspendisse potenti. Vivamus quis nisl sit amet nisl tincidunt consequat. Phasellus quis lorem sed lorem condimentum tincidunt. Proin quis nisl quis lorem sagittis sollicitudin. Sed at nisl quis lorem fermentum luctus. Donec quis nisl quis lorem consequat tincidunt. Nulla quis nisl quis lorem sagittis sollicitudin. Sed at nisl quis lorem fermentum luctus. Donec quis nisl quis lorem consequat tincidunt. Nulla quis nisl quis lorem sagittis sollicitudin. Sed at nisl quis lorem fermentum luctus.
